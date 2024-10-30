1035fac4c60ab94606415887617f35ae333f8292
Juan <juansepacheco@hotmail.com>
2024-10-30 11:42:55 +0100

    Add .gitignore and config example

--- bot/config.py
+++
@@ -1,5 +0,0 @@
-strava_id = "1111" # This is Strava's Client ID
-strava_secret = "111" # This is Strava's Client Secret
-api_id = "1111" # This is Telegram API ID
-api_hash = "1111" # This is Telegram API hash
-bot_token = "111" # This is the bot token from Botfathe
