0a7d004306edf186e7f4bf26a86f9d7d50c517db
Juan <juansepacheco@hotmail.com>
2024-10-30 11:38:06 +0100

    sample config file

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,5 @@
-strava_id = "65456456" # This is Strava's Client ID
-strava_secret = "46654564" # This is Strava's Client Secret
-api_id = "54654654" # This is Telegram API ID
-api_hash = "654654564564" # This is Telegram API hash
-bot_token = "4654564654" # This is the bot token from Botfathe
+strava_id = "1111" # This is Strava's Client ID
+strava_secret = "111" # This is Strava's Client Secret
+api_id = "1111" # This is Telegram API ID
+api_hash = "1111" # This is Telegram API hash
+bot_token = "111" # This is the bot token from Botfathe
